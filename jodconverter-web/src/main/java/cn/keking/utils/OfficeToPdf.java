191f07482a61ba43c963930c7cfc5570db0a32ff
Congliang0229 <fanpan@fanpandeMacBook-Pro.local>
2020-11-23 17:08:47 +0800

    utils first commit

--- jodconverter-web/src/main/java/cn/keking/utils/OfficeToPdf.java
+++ jodconverter-web/src/main/java/cn/keking/utils/OfficeToPdf.java
@@ -4,7 +4,10 @@ import org.artofsolving.jodconverter.OfficeDocumentConverter;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
-
+/*
+ *Author:FanPan Date:2020-11-17
+ *将office文件转换为pdf的工具类
+ */
 /**
  * @author yudian-it
  */
@@ -12,15 +15,27 @@ import java.io.File;
 public class OfficeToPdf {
     private final ConverterUtils converterUtils;
 
+
     public OfficeToPdf(ConverterUtils converterUtils) {
         this.converterUtils = converterUtils;
     }
 
+    /**
+     *调用office2pdf，依然是转换为pdf
+     * @Author FanPan
+     * @Date 2020-11-19
+     *
+     */
     public void openOfficeToPDF(String inputFilePath, String outputFilePath) {
         office2pdf(inputFilePath, outputFilePath);
     }
 
-
+    /**
+     *实现最终的转换，被office2pdf调用
+     * @Author FanPan
+     * @Date 2020-11-19
+     *
+     */
     public static void converterFile(File inputFile, String outputFilePath_end,
                                      OfficeDocumentConverter converter) {
         File outputFile = new File(outputFilePath_end);
@@ -31,7 +46,13 @@ public class OfficeToPdf {
         converter.convert(inputFile, outputFile);
     }
 
-
+    /**
+     * @Author FanPan
+     * @Date 2020-11-19
+     * @param inputFilePath
+     * @param outputFilePath
+     * 将office文件转换为pdf
+     */
     public void office2pdf(String inputFilePath, String outputFilePath) {
         OfficeDocumentConverter converter = converterUtils.getDocumentConverter();
         if (null != inputFilePath) {
